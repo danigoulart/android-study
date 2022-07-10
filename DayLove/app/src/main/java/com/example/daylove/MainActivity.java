4f4a2106031e5c64c2203d2db722ec0da91e328e
Daniela Goulart <goulartsdani@gmail.com>
2022-07-10 15:08:52 -0300

    App gerador de frases

--- DayLove/app/src/main/java/com/example/daylove/MainActivity.java
+++ DayLove/app/src/main/java/com/example/daylove/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.daylove;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarNovaFrase(View view){
+        String[] frases = {
+          "frase 1",
+          "frase 2",
+          "frase 3"
+        };
+
+        int numero = new Random().nextInt(4);
+
+        TextView texto = findViewById(R.id.textResultado);
+        texto.setText( frases[numero]);
+    };
+
 }
